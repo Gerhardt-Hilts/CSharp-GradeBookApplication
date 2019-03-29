40908c2ff1dd8baafd4c0c950dbafbfcb9d56e0c
Garry-H <gerhardthilts@gmail.com>
2019-03-29 04:03:10 -0700

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,9 +22,9 @@ namespace GradeBook.GradeBooks
                 return 'A';
             else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[(threshold * 3) - 2] <= averageGrade)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[(threshold * 4) - 3] <= averageGrade)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             return 'F';
         }
